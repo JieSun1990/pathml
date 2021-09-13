b13edf4254234c49d1f74e27a0e5f492f4eea5cf
JieSun1990 <jie.sun1990@gmail.com>
2021-09-12 21:31:21 -0400

    d

--- pathml/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Copyright 2021, Dana-Farber Cancer Institute and Weill Cornell Medicine
-License: GNU GPL 2.0
-"""
-
-from .core import *
-from . import datasets as ds
-from . import ml
-from . import preprocessing as pp
